30218e1994aea5ccb3f9e93daa67bb3869ae7e2a
Roman Ojha <razzroman98@gmail.com>
2022-03-16 16:46:20 +0545

    solidity compilation, mainnet vs testnet

---
+++ Class/05_Mainnet_Vs_Testnet/05_Mainnet_Vs_Testnet.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/*
+    *) Mainnet
+        -> Used for actual transactions with value.
+        -> Mainnet's network ID is 1
+        -> Example - Ethereum mainmet
+        -> https://etherscan.io/blocks
+    *) Testnet
+        -> Testnet User for testing smart contract and decentralized applications
+        -> Testnets have network IDs of 3,4,42
+        -> Example - Rinkeby Test Network 
+        -> https://www.rinkeby.io/#stats
+
+    -> https://masterthecrypto.com/mainnet-vs-testnet-whats-the-difference/     
+*/
