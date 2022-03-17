1905a444a59038dcbab23fec26f21a380f9f2e7d
Roman Ojha <razzroman98@gmail.com>
2022-03-17 10:40:55 +0545

    import contract, inheritance

---
+++ Class/32_Inheritance/StorageFactory.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory is SimpleStorage {
+    // here we are inheriting from the 'SimpleStorage' Contract now this contract include all the function that are defined in 'SimpleStorage'
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
+        SimpleStorage simpleStorage = SimpleStorage(
+            address(simpleStorageArray[_simpleStorageIndex])
+        );
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage simpleStorage = SimpleStorage(
+            address(simpleStorageArray[_simpleStorageIndex])
+        );
+        return simpleStorage.retrive();
+    }
+}
