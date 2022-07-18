0423ece93ca75252f52ad19ecfc2cf6101707834
Roman-Ojha <razzroman98@gmail.com>
2022-07-18 11:14:04 +0545

    feat(practice02): calculator constructor

---
+++ Practice/contracts/Practice02.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract Practice02Calculator {
+    int256 a;
+    int256 b;
+    int256 c;
+
+    constructor(int256 _x, int256 _y) {
+        a = _x;
+        b = _y;
+        c = 0;
+    }
+}
