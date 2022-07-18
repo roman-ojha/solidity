71709887a198c24e88364dfa74c344278a9563b6
Roman-Ojha <razzroman98@gmail.com>
2022-07-18 11:20:38 +0545

    feat(practice02): calculator add, subtract, multiply, divide

--- Practice/contracts/Practice02.sol
+++ Practice/contracts/Practice02.sol
@@ -3,13 +3,29 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract Practice02Calculator {
-    int256 a;
-    int256 b;
     int256 c;
 
-    constructor(int256 _x, int256 _y) {
-        a = _x;
-        b = _y;
+    constructor() {
         c = 0;
+    }
+
+    function getC() public view returns (int256) {
+        return c;
+    }
+
+    function add(int256 _x, int256 _y) public {
+        c = _x + _y;
+    }
+
+    function subtract(int256 _x, int256 _y) public {
+        c = _x - _y;
+    }
+
+    function multiply(int256 _x, int256 _y) public {
+        c = _x * _y;
+    }
+
+    function divide(int256 _x, int256 _y) public {
+        c = _x / _y;
     }
 }
